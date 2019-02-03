6a3b6f4765be28199630590965af61cfcf029b8e
CobaltXII <sidatt64@gmail.com>
2019-02-03 14:06:00 -0500

    Implemented enum huffman_tree_node_type

--- huffman.cpp
+++ huffman.cpp
@@ -7,3 +7,12 @@
 #include <bitset>
 #include <locale>
 #include <map>
+
+// A Huffman tree node may be an internal node or a leaf node. Internal nodes
+// represent a connection between two Huffman tree nodes, and leaf nodes
+// represent a glyph from the original plaintext's alphabet.
+
+enum huffman_tree_node_type
+{
+	huffman_internal, huffman_leaf
+};
